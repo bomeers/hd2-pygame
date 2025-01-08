a6484938514102542791bcbc195d23ea15f8ca45
bomeers <bmeers5@gmail.com>
2025-01-07 20:26:10 -0600

    - refined dragging logic - added mars sprite - getting biomes translated

--- helldivers.py
+++ helldivers.py
@@ -15,16 +15,19 @@ background_initial_rect = background_rect.copy()
 
 sprite_sheet = pygame.image.load('war-table-sprite-sheet.png')
 
-super_earth_sprite = pygame.Rect(335, 0, 173, 201)  # (x, y, width, height) 
-super_earth_rect = sprite_sheet.subsurface(super_earth_sprite)
-super_earth_image = pygame.transform.scale(super_earth_rect, (88, 100))
+super_earth_sprite = pygame.Rect(335, 0, 173, 201) # (x, y, width, height)
+super_earth_image = pygame.transform.scale(sprite_sheet.subsurface(super_earth_sprite), (88, 100))
 super_earth_image_rect = super_earth_image.get_rect()
 super_earth_image_rect.center = background_rect.center
+
+mars_sprite = pygame.Rect(268, 201, 67, 67) # (x, y, width, height)
+mars_image = pygame.transform.scale(sprite_sheet.subsurface(mars_sprite), (50, 50))
+mars_image_rect = mars_image.get_rect()
 
 click_time = 0
 dragging = False
 zoomed = False  
-offset_x, offset_y = 0, 0
+offset = (0,0)
 
 # hide cursor
 # bitmask = [0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,]
@@ -44,8 +47,7 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if zoomed:
                 dragging = True
-                offset_x = background_rect.x - event.pos[0]
-                offset_y = background_rect.y - event.pos[1]
+                offset = (background_rect.x - event.pos[0], background_rect.y - event.pos[1])
 
         # Mouse button up: Stop dragging
         elif event.type == pygame.MOUSEBUTTONUP:
@@ -64,15 +66,10 @@ while True:
         # Mouse motion: Update position if dragging
         elif event.type == pygame.MOUSEMOTION:
             if dragging:
-                background_rect.x = event.pos[0] + offset_x
-                background_rect.y = event.pos[1] + offset_y
-
-                # Prevent the image from being dragged out of bounds
-                # ({background width or height}/2) - ({image width or height}/2))
-                super_earth_image_rect.x = background_rect.x + 918 #((480/2)-(super_earth_image_rect.x/2)) # 918
-                super_earth_image_rect.y = background_rect.y + 910 #((480/2)-(super_earth_image_rect.y/2)) # 910
+                background_rect.topleft = (event.pos[0] + offset[0], event.pos[1] + offset[1])
                 
-                # ({background width or height} - {screen size}), set right & bottom to left-over number ?
+                # Prevent the image from being dragged out of bounds
+                # Calculation: ({background width or height} - {screen size}), set right & bottom to left-over number ?
                 if background_rect.left < -1440:
                     background_rect.left = -1440
                 if background_rect.top < -1440:
@@ -82,11 +79,21 @@ while True:
                 if background_rect.bottom > 480:
                     background_rect.bottom = 480
 
+                # Calculation: ({background width or height}/2) - ({image width or height}/2)) EX ((480/2)-(super_earth_image_rect.x/2))
+                super_earth_image_rect.topleft = (background_rect.x + 918, background_rect.y + 910)
+                mars_image_rect.topleft = (background_rect.x + 1000, background_rect.y + 900)
+
+                # PLANETS LOOP HERE
+                # store planets in list of objects called "planets"
+                # for i in planets:
+                #   - get sprite coordinates
+                #   - get x y defaults
+                #   - get and set new x y coordinates
+
     screen.blit(background, background_rect)
     screen.blit(super_earth_image, super_earth_image_rect)
-    # screen.blit(super_earth_image, (240, 240))
-    # screen.blit(sprite_image, (300, 300))
-    # screen.blit(sprite_sheet, (0, 0))
+    if zoomed == True:
+        screen.blit(mars_image, mars_image_rect)
     
     pygame.display.update()
 
@@ -94,3 +101,7 @@ while True:
 # - add double click buffer time to prevent unwanted zoom level on triple clicks.
 # - find way to display which faction owns what sector (API)
 # - create list of planet "objects", each with multiple updatable properties. (Eventually import this data from API?)
+# API
+# - use /api/v1/planets to get list of planets and biomes for images
+# - join /api/v1/war/status by index to get x y position and player count
+# - use /api/v1/war/campaign to get current events and 
